e2950ee26feece824f5ef97ef82b55fb02ba5920
Melanie N <melanie.nachef@gmail.com>
2020-06-26 17:46:25 +0200

    set up

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
+require('dotenv').config();
+
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const bodyParser = require('body-parser');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -14,11 +17,47 @@ app.use(cors({
     credentials: true
 }));
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
+const mongoose = require('mongoose');
+
+app.use(session({
+    secret: process.env.secret,
+    resave: false,
+    saveUninitialized: true,
+    store: new MongoStore({ mongooseConnection: mongoose.connection })
+}));
+
+mongoose
+    .connect(process.env.db, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => {
+        console.log('Connected to the database');
+    })
+    .catch(err => {
+        console.log(err);
+    });
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+// Protect Middleware
+function protectMiddleWare(req,res,next){
+    console.log("Protect Middleware called");
+    if(req.session.user){
+        next();
+    } else {
+        res.redirect("/users/login");
+    }
+}
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
