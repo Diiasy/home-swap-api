27ba23c752a5a315c28933f38a3eac347a08b1ef
Melanie N <melanie.nachef@gmail.com>
2020-06-26 13:18:46 +0200

    initialisation api

---
+++ app.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+var cors = require('cors');
+app.use(cors({
+    origin: true, //put the site address to make it more safe ["url"]
+    credentials: true
+}));
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+module.exports = app;
